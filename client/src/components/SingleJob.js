d5d42538075aaa59a3e3b0d3491cf4193632050d
Michael Beale <michael@bealefamily.net>
2025-11-05 16:46:47 -0800

    refactor: Remove unused imports from SingleJob component

--- client/src/components/SingleJob.js
+++ client/src/components/SingleJob.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+import React from "react";
 import SingleJobCard from "./SingleJobCard";
 
 const SingleJob = ({ jobId: propJobId, onClose }) => {
